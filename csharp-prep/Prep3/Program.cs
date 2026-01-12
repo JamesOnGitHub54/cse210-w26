1987ddb6298ec0c0e52f97f1e0c6eac65381b52a
JamesOnGitHub54 <jamesg521@outlook.com>
2026-01-12 04:36:22 -0700

    Completed core requirements.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,12 +5,17 @@ class Program
     static void Main(string[] args)
     {
         // Magic number established by user.
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Magic number is generated.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
         // Initialized user guess will differ from magic number.
         int userGuess = magicNumber + 1;
 
+        // Game loops until number is guessed.
         while (userGuess != magicNumber)
         {
             // User guess is made.
