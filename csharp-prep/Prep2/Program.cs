b87f00da046e1d836026adb081bac4a617ba7ec0
JamesOnGitHub54 <jamesg521@outlook.com>
2026-01-12 03:47:09 -0700

    Completed core requirement 1.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        // Determine and print grade.
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Grade: F");
+        }
     }
 }
