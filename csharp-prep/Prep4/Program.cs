05e2a7d490df776443d52868d608722bb3999e1d
JamesOnGitHub54 <jamesg521@outlook.com>
2026-01-12 04:52:32 -0700

    Completed core requirement 1.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine ("Enter a list of numbers, type 0 when finished.");
+
+        // Add to list until 0 is entered.
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Sum is calculated.
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
