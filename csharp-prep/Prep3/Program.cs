79ec41b385fd500c13d8ef7c48ca96da911fe3a0
JamesOnGitHub54 <jamesg521@outlook.com>
2026-01-12 04:15:24 -0700

    Completed core requirement 1.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Magic number established by user.
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // User guess made.
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
+
+        // Determine guess to magic number relationship. Gives hint or completion message.
+        if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
