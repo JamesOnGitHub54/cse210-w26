a17e3f1996983d4e7cf8369da1422f06a3b705f9
JamesOnGitHub54 <jamesg521@outlook.com>
2026-01-12 03:30:09 -0700

    Created basic structure for Prep1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Asking for user's name
+        Console.WriteLine("What is your first name? ");
+        Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        Console.ReadLine();
+
+        // Display user's name in assignment format.
+        Console.WriteLine("Your name is ");
     }
 }
